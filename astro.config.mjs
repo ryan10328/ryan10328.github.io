5a9a0c8bb7f2ce22e88b8a037182503210f3be03
ryan.tseng <ryan10328@gmail.com>
2024-05-17 01:34:32 +0800

    added back orginal setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import {pluginLineNumbers} from "@expressive-code/plugin-line-numbers";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://blog.ryantseng.me',
-    outDir: './docs',
     integrations: [
         expressiveCode({
             plugins: [pluginLineNumbers()],

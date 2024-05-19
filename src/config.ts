95c27da589393d2b358f4fae21580356fda0e8da
ryan.tseng <ryan10328@gmail.com>
2024-05-19 16:08:18 +0800

    change site description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ryan | 不務正業 全端開發記事';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = '早期專注於 .NET 為主的技術，直到某次開始不小心碰到前端的框架，一不小心就栽進去，目前前端部分主要以 Vue 開發為主，但同時也不斷接觸各種不同的技術，持續不斷的學習。';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
